5b775d7f10fe2e3879ed824d292c3273ff99b18c
Michaela Semanová <44406479+Michaela1124@users.noreply.github.com>
2019-01-09 01:52:51 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+    }
+
+function pause(){
+    var audio = document.getElementById("audio");
+    audio.pause();
+    }
+
+const wolf = document.querySelector(".logo1");
+wolf.addEventListener("click",show);
+
+function show(){
+    document.querySelector('.logotext').style.animation = "wolf 3s forwards";
+    }
